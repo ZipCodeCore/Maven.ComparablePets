37fa55e35195d5ab15f57c3d4016dd1c8a4eade0
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-11-06 14:30:00 -0500

    sortTest

--- src/test/io/zipcoder/pets/MainTest.java
+++ src/test/io/zipcoder/pets/MainTest.java
@@ -1,7 +1,34 @@
 package io.zipcoder.pets;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
+@Test
+    public void sortingTest(){
 
+    ArrayList<Pets> userPets = new ArrayList<Pets>();
+
+    userPets.add(new Fish("joe"));
+    userPets.add(new Dog("joe"));
+    userPets.add(new Cat("joe"));
+    userPets.add(new Cat("zebra"));
+
+    for (Pets pets : userPets) {
+        System.out.println(pets.getName() + " goes " + pets.getClass().getName());
+    }
+
+    System.out.println("\n");
+
+    Collections.sort(userPets);
+
+    for (Pets pets : userPets) {
+        System.out.println(pets.getName() + " goes " + pets.getClass().getName());
+    }
+
+}
 }
