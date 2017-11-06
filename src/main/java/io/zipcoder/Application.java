c612fd36d2357a9d0228869dfa828fb4408d7725
logan <loganhanner@gmail.com>
2017-11-06 16:14:52 -0500

    Completed lab

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,68 @@
 package io.zipcoder;
 
+import io.zipcoder.pets.Cat;
+import io.zipcoder.pets.Dog;
+import io.zipcoder.pets.Falcon;
+import io.zipcoder.pets.Pet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Application {
+
+    public static void main(String[] args){
+
+        ArrayList<Pet> pets = new ArrayList<Pet>();
+//        String storage = "";
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("How many pets do you have?");
+        int numberOfPets = keyboard.nextInt();
+        keyboard.nextLine();
+
+        for(int i = 1; i <= numberOfPets; i++) {
+            System.out.println("What type of pet is pet number " + i + "?");
+            String type = keyboard.nextLine();
+
+            if("dog".equalsIgnoreCase(type)) {
+
+                System.out.println("What is the name of your dog?");
+                String name = keyboard.nextLine();
+
+                pets.add(new Dog(name));
+            }
+            else if ("cat".equalsIgnoreCase(type)) {
+                System.out.println("What is the name of your cat?");
+                String name = keyboard.nextLine();
+
+                pets.add(new Cat(name));
+            }
+            else if("falcon".equalsIgnoreCase(type)) {
+                    System.out.println("HOW WOULD YOU NAME SUCH A MAJESTIC BEAST?");
+                    String name = keyboard.nextLine();
+
+                pets.add(new Falcon(name));
+            }
+            else {
+                continue;
+            }
+
+        }
+
+        Collections.sort(pets);
+
+        for (Pet pet : pets) {
+            System.out.print(pet.getName() + " says, ");
+            pet.speak();
+        }
+
+
+    }
+
+
+
 }
+
--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,5 +1,84 @@
 package io.zipcoder;
 
 
+import io.zipcoder.pets.Cat;
+import io.zipcoder.pets.Falcon;
+import io.zipcoder.pets.Dog;
+import io.zipcoder.pets.Pet;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ApplicationTest {
+
+    ArrayList<Pet> pets = new ArrayList<>();
+
+    String catName = "Mr. Mittens";
+    String dogName = "Dog Jr.";
+    String falconName = "Falco";
+
+    Cat kitty = new Cat(catName);
+    Cat jim = new Cat("Jim");
+    Dog puppy = new Dog(dogName);
+    Falcon peregrine = new Falcon(falconName);
+
+    @Test
+    public void getNameTest() {
+        String expected = catName;
+
+        String actual = kitty.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setNameTest() {
+        String expected = "Dr, Chaos";
+
+        puppy.setName(expected);
+
+        String actual = puppy.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void speakTest() {
+        peregrine.speak();
+    }
+
+    @Test
+    public void sortTest_SameType() {
+        pets.add(new Cat("Zzz"));
+        pets.add(kitty);
+        pets.add(jim);
+        Collections.sort(pets);
+
+        String expected = "[Jim, Mr. Mittens, Zzz]";
+
+        String actual = pets.toString();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sortTest_DifferentType() {
+        pets.add(peregrine);
+        pets.add(puppy);
+        pets.add(jim);
+        pets.add(kitty);
+
+        Collections.sort(pets);
+
+        String expected = "[Jim, Mr. Mittens, Dog Jr., Falco]";
+
+        String actual = pets.toString();
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
