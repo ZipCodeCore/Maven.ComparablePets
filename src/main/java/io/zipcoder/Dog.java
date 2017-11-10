0b28e166193b5b6590a0e0e90afdc72d7f032284
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-10 10:23:06 -0500

    All finished

---
+++ src/main/java/io/zipcoder/Dog.java
@@ -0,0 +1,14 @@
+package io.zipcoder;
+
+public class Dog extends Pet {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public String speak() {
+        return "bark!";
+    }
+
+}
