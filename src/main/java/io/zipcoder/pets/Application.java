f55d85f0c96f4976d6071ac9e06c59a1fb991ab0
Carolynn Vansant <cvansant@MacBook-Pro-5.fios-router.home>
2018-02-26 17:12:03 -0500

    first commit, created classes and test classes

---
+++ src/main/java/io/zipcoder/pets/Application.java
@@ -0,0 +1,21 @@
+package io.zipcoder.pets;
+
+
+import io.zipcoder.pets.Pets;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Application {
+
+    private Pets[] petList;
+    private String nameOfPet;
+    private String typeOfPet;
+    private int numberOfPets;
+
+    public Application() {
+        this.petList = petList;
+
+    }
+
+}
+
---
+++ src/test/java/io/zipcoder/pets/ApplicationTest.java
@@ -0,0 +1,18 @@
+package io.zipcoder.pets;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ApplicationTest {
+
+    @Test
+    public void constructorTest() {
+
+    }
+
+    @Test
+    public void storePetList() {
+
+    }
+
+}
