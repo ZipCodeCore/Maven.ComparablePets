85d694c58bf58991a7e4f955e876e8f948c982ff
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-02-26 16:01:06 -0500

    maybe

--- src/test/java/io/zipcoder/CatTest.java
+++ src/test/java/io/zipcoder/CatTest.java
@@ -1,7 +1,32 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CatTest {
+
+    @Test
+    public void getNameOfCatsTest() {
+
+        Cat cat = new Cat();
+        cat.setName("Felix");
+        String expected = "Felix";
+        String actual = cat.getName();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void catSpeakTest() {
+        Cat cat = new Cat();
+        String expected = "meow";
+        String actual = cat.speak();
+
+        Assert.assertEquals(expected, actual);
+
+    }
 
 }
--- src/test/java/io/zipcoder/SnakeTest.java
+++ src/test/java/io/zipcoder/SnakeTest.java
@@ -1,7 +1,40 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SnakeTest {
+
+    @Before
+    public void setUp() {
+    }
+
+
+    @Test
+    public void getNameOfSnakesTest() {
+
+        Snake snake = new Snake();
+        snake.setName("Luke");
+        String expected = "Luke";
+        String actual = snake.getName();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void snakeSpeakTest() {
+        Snake snake = new Snake();
+        String expected = "hiss";
+        String actual = snake.speak();
+
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
 
 }
