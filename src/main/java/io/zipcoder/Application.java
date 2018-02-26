5660a350f74959199e01cc87c8b0db2835770516
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-02-26 18:14:45 -0500

    notes

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -2,4 +2,26 @@ package io.zipcoder;
 
 
 public class Application {
+
+    //constructors
+
+    //methods
 }
+
+
+//
+//        Overview
+//    1. how many pets do you have?
+//        1.1. what kind of pet is each?
+//        1.2 what is the pets name?
+//        1.3 print list/overview
+//
+//
+//    2. create Pet class
+//            -has speak method
+//            -has getter & setter methods
+//        2.1 create subclass for each type of pet(subclasses3)
+//                -Dog, Cat, Other?
+                        // subclasses have unique speak method()
+
+
--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -2,4 +2,8 @@ package io.zipcoder;
 
 
 public class ApplicationTest {
+          // App requirements / Must test
+         //getters & setters (names) (pet class)
+        // speak method
+        //
 }
