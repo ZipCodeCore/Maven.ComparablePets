b1ab3a8f85be282fbe908ed900c6a7f633702754
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-26 14:56:42 -0500

    commit

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,10 @@
 package io.zipcoder;
 
 
+import com.sun.javafx.geom.Edge;
+
+
 public class Application {
+
+
 }
---
+++ src/main/java/io/zipcoder/pets/Pet.java
@@ -0,0 +1,13 @@
+package io.zipcoder.pets;
+
+public class Pet {
+    String name;
+
+    public void setName(){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
