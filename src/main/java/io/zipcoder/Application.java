8ed079b4ad28f7bca3fd2f7e2dd9c354040311b4
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-02-26 22:08:43 -0500

    created a solid layout for application

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,31 @@
 package io.zipcoder;
 
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application {
+    public static void main(String[] args){
+
+        ArrayList<String> petMap = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many pets do you have?");
+        Integer numOfPets = sc.nextInt();
+        sc.nextLine();
+
+        for(Integer i = 0; i<numOfPets; i++){
+            System.out.println("For each pet, what kind of pet is it?");
+            String pet = sc.nextLine();
+            System.out.println("What's the name of your pet?");
+            String petName = sc.nextLine();
+            petMap.add(pet);
+            petMap.add(petName);
+
+        }
+
+        for(String i : petMap){
+            System.out.printf("This pet is a %s need %s%n", i, petMap.get(Integer.parseInt(i)));
+        }
+    }
 }
--- src/test/java/io/zipcoder/ApplicationTest.java
+++ src/test/java/io/zipcoder/ApplicationTest.java
@@ -1,5 +1,16 @@
 package io.zipcoder;
 
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ApplicationTest {
+
+    @Test
+    public void
+
+
+
 }
