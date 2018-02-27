6ed5c34592abd2f81de7995352e41d5f0fbb08b6
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-02-26 23:08:47 -0500

    polymorphism and interfaces completed. Need to add more testing

---
+++ src/main/java/io/zipcoder/Reptile.java
@@ -0,0 +1,13 @@
+package io.zipcoder;
+
+public class Reptile extends PetClass {
+
+    public Reptile(String name) {
+        super(name);
+    }
+
+    @Override
+    public String speak() {
+        return "Hiss!";
+    }
+}
---
+++ src/test/java/io/zipcoder/ReptileTest.java
@@ -0,0 +1,30 @@
+package io.zipcoder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReptileTest {
+
+    @Test
+    public void testSpeak() {
+        Reptile myReptile = new Reptile("Alfred");
+        String expected = "Hiss!";
+        String actual = myReptile.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetName() {
+        Reptile myReptile = new Reptile("Alfred");
+        String expected = "Alfred";
+        String actual = myReptile.getName();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSetName() {
+    }
+}
