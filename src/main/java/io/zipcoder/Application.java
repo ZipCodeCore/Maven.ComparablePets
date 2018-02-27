81aa33bda7b4e59c9ce9428158ad1db28ce0f643
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-27 14:29:52 -0500

    updates

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -1,5 +1,33 @@
 package io.zipcoder;
-
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Application {
+    public static void main(String[] args) {
+
+        HashMap<String, String> petMap = new HashMap<String, String>();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many pets do you have ?");
+        int numberOfpets =scan.nextInt();
+        scan.nextLine();
+
+        //going through the loop and setting it equal to number of pets.
+        for (int i =0; i <numberOfpets; i++){
+            System.out.println("What kind of pet do you have ?");
+            //waits for input to be entered before continuing on to the next line.
+            String pet = scan.nextLine();
+            System.out.println("What is the name of your pet?");
+            String petName = scan.nextLine();
+            petMap.put(pet,petName);
+
+        }
+        //.keySet() returns all the keys in the hashmap, (petMap)
+
+        for (String i : petMap.keySet()){
+            System.out.printf("This pet is a %s named %s%n", i,petMap.get(i));
+        }
+
+
+    }
 }
